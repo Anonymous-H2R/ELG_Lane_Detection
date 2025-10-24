2b25d8961843a0db4834b80abc99bd275b73a07e
Anonymous-H2R <hitik072001@gmail.com>
2025-10-24 11:39:19 -0400

    Tusimple dataset uploaded

--- dataset/tusimple_download.py
+++ dataset/tusimple_download.py
@@ -1,6 +1,35 @@
+# import kagglehub
+#
+# # Download latest version
+# path = kagglehub.dataset_download("manideep1108/tusimple")
+#
+# print("Path to dataset files:", path)
+import os,glob
 import kagglehub
+import shutil
 
 # Download latest version
-path = kagglehub.dataset_download("manideep1108/tusimple")
+path = kagglehub.dataset_download("rangalamahesh/preprocessed-1")
 
 print("Path to dataset files:", path)
+
+
+
+for root, dirs, files in os.walk(path):
+    if files:
+        for f in files[:10]:
+            print(os.path.join(root, f))
+        break
+
+# Change path of the folders where you downloaded the dataset below path won't work on your system
+# src_folder = r"\kagglehub\datasets\rangalamahesh\preprocessed-1\versions\1\training\kaggle\working\tusimple_preprocessed\training\frames"
+# dst_folder = r"ELG_Lane_Detection\dataset\tusimple"
+#
+# shutil.move(src_folder, dst_folder)
+# print("Moved dataset successfully.")
+
+# AFter moving the dataset to tusimple folder check if its correctly moved add your own path below path won't work on your system
+# sample_folder = r"ELG_Lane_Detection\dataset\tusimple\frames"
+# imgs = sorted(glob.glob(os.path.join(sample_folder, "*.jpg")))
+# print("Found images:", len(imgs))
+# print("First 5:", imgs[:5])
---
+++ src/config.py
@@ -0,0 +1,4 @@
+# src/config.py
+DATASET_TUSIMPLE = r"C:\Users\Dell\PycharmProjects\ELG_Lane_Detection\dataset\tusimple_samples"
+DATASET_CULANE  = r"C:\Users\Dell\PycharmProjects\ELG_Lane_Detection\dataset\culane_small"  # optional
+OUTPUT_DIR = r"C:\Users\Dell\PycharmProjects\ELG_Lane_Detection\results"
